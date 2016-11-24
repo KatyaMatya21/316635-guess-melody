ff18850b70d556ff114b77ee67d911a0350d904f
Katya <katyamatya21@gmail.com>
2016-11-25 00:55:51 +0300

    Привет Eslint

--- js/modules/logo.js
+++ js/modules/logo.js
@@ -1,1 +1,1 @@
-export default `<section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>`;
+export default '<section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>';
--- js/modules/main--result.js
+++ js/modules/main--result.js
@@ -11,7 +11,7 @@ export default (data) => {
                     <div class="main-stat">За&nbsp;${data.stat.minute}&nbsp;минуты<br>вы&nbsp;отгадали ${data.stat.songs}&nbsp;мелодии</div>
                     <span class="main-comparison">Это&nbsp;лучше чем у&nbsp;${data.stat.compare}&nbsp;игроков</span>`;
 
-  const playButtonHtml = `<span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>`;
+  const playButtonHtml = '<span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>';
 
   const article = `<section class="main main--result">
                   ${logo}
